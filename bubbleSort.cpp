c774e7bb4ae16c6d0a05f56a73ebfe07f81b143d
Jimi Chen <superchild@JimideMacBook-Pro.local>
2023-03-15 22:47:36 +0800

    Add bubble sort

---
+++ bubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(vector<int> &input) {
+    int size = input.size();
+
+    for (int step=0; step<size-1; step++) {
+        for (int i=0; i<size-step-1; i++) {
+            if (input[i] > input[i+1]) {
+                swap(input[i], input[i+1]);
+            }
+        }
+    }
+}
+
+void printArray(vector<int> &input) {
+    for (int i=0; i<input.size(); i++) {
+        cout << input[i] << ' ';
+    }
+
+    cout << endl;
+}
+
+int main() {
+    vector<int> input = {-1, 0, -45, 2, -8, 56, 99, 3, 7};
+    printArray(input);
+    bubbleSort(input);
+    printArray(input);
+}

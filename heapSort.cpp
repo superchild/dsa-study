091d875e01013b64d0ad314252d3231afd676e39
Jimi Chen <superchild@JimideMacBook-Pro.local>
2023-03-15 23:01:02 +0800

    Add heap sort

---
+++ heapSort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void heapify(vector<int> &input, int idx, int end) {
+    int maxIdx = idx;
+    int leftIdx = 2*idx+1;
+    int rightIdx = 2*idx+2;
+
+    if (leftIdx < end && input[maxIdx] < input[leftIdx]) {
+        maxIdx = leftIdx;
+    }
+
+    if (rightIdx < end && input[maxIdx] < input[rightIdx]) {
+        maxIdx = rightIdx;
+    }
+
+    if (maxIdx != idx) {
+        swap(input[maxIdx], input[idx]);
+        heapify(input, maxIdx, end);
+    }
+}
+
+void heapSort(vector<int> &input) {
+    int size = input.size();
+
+    // build max heap first, start from non-leaf node bottom up
+    for (int i=size/2-1; i>=0; i--) {
+        heapify(input, i, size);
+    }
+
+    // each time swap root and the last node, regard it as ready from the end
+    // then heapify again
+    for (int i=size-1; i>=0; i--) {
+        swap(input[0], input[i]);
+        heapify(input, 0, i);
+    }
+
+}
+
+void printArray(vector<int> &input) {
+    for (int i=0; i<input.size(); i++) {
+        cout << input[i] << ' ';
+    }
+
+    cout << endl;
+}
+
+int main() {
+    vector<int> input = {-1, 0, -45, 2, -8, 56, 99, 3, 7};
+    printArray(input);
+    heapSort(input);
+    printArray(input);
+}

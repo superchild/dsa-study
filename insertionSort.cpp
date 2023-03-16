721581dad39d82e1f178389cf282fb7307b4b1bc
jimi_chen <jimi_chen@edge-core.com>
2023-03-16 11:44:26 +0800

    Add insertion sort

---
+++ insertionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void insertionSort(vector<int> &input) {
+    int size = input.size();
+    for (int i=1; i<size; i++) {
+        int key = input[i];
+        int j=i-1;
+        
+        while (j>=0 && input[j] > key) {
+            input[j+1] = input[j];
+            j--;
+        }
+        
+        input[j+1] = key;
+    }
+}
+
+void printArray(vector<int> &input) {
+    for (int i=0; i<input.size(); i++) {
+        cout << input[i] << ' ';
+    }
+
+    cout << endl;
+}
+
+int main() {
+    vector<int> input = {-1, 0, -45, 2, -8, 56, 99, 3, 7};
+    printArray(input);
+    insertionSort(input);
+    printArray(input);
+}

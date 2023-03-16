12838e6e8109bec394a338d3a9171819451faf75
jimi_chen <jimi_chen@edge-core.com>
2023-03-16 15:05:53 +0800

    Add selection sort

---
+++ selectionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void selectionSort(vector<int> &input) {
+    int size = input.size();
+    
+    for (int i=0; i<size; i++) {
+        int minIdx = i;
+        for (int j=i+1; j<size; j++) {
+            if (input[minIdx] > input[j]) {
+                minIdx = j;
+            }
+        }
+        
+        swap(input[minIdx], input[i]);
+    }
+}
+
+void printArray(vector<int> &input) {
+    for (int i=0; i<input.size(); i++) {
+        cout << input[i] << ' ';
+    }
+
+    cout << endl;
+}
+
+int main() {
+    vector<int> input = {-1, 0, -45, 2, -8, 56, 99, 3, 7};
+    printArray(input);
+    selectionSort(input);
+    printArray(input);
+}

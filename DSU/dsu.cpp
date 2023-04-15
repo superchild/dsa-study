0f6110a3465e0ca303f213120fed4da837587810
Jimi Chen <flyseed426@gmail.com>
2023-04-15 16:08:53 +0800

    add disjion set union

---
+++ DSU/dsu.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+class DSU {
+    public:
+        DSU(int num) {
+            roots.resize(num);
+
+            // initialize each index's root as itself
+            for (int i=0; i<num; i++) {
+                roots[i] = i;
+            }
+
+            // it can use STL instead
+            // iota(roots.begin(), roots.end(), 0);
+        }
+
+        // find the root index of current index
+        int find(int idx) {
+            if (roots[idx] == idx) {
+                return idx;
+            }
+
+            // path reduction
+            roots[idx] = find(roots[idx]);
+            return roots[idx];
+        }
+
+        // join two index together, set their root index as the same
+        void join(int lhs, int rhs) {
+            int lhs_root = find(lhs);
+            int rhs_root = find(rhs);
+
+            if (lhs_root == rhs_root) {
+                return;
+            }
+
+            // choose minimm as root
+            int min_root = min(lhs_root, rhs_root);
+            roots[lhs] = min_root;
+            roots[rhs] = min_root;
+        }
+
+        vector<int> roots;  // record the root index of current index
+};
